159d9f23650e5dcd96a3b9450ddd7548356e0d1c
Diego Avedissian <61209163+davedissian@users.noreply.github.com>
2020-10-27 21:07:10 -0300

    Adicionando CSS, JS, SQL e PHP

---
+++ loja.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loja</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+
+<body>
+    <?php
+    include('cabecalho.html');
+    ?>
+    <section id="lojas" class="lojas">
+        <div class="loja">
+            <h2>Loja 1</h2>
+            <h2>Rio de Janeiro</h2>
+            <h4>Avenida Realengo, 1952</h>
+                <h4>Realengo</h4>
+                <h4>Tel: (21)1111-6987</h4>
+        </div>
+        <div class="loja">
+            <h2>Loja 2</h2>
+            <h2>São Paulo</h2>
+            <h4>Avenida Assis Ribeiro, 758</h4>
+            <h4>Ermelino Matarazo</h4>
+            <h4>Tel: (11) 2222-3333</h4>
+        </div>
+        <div class="loja">
+            <h2>Loja 3</h2>
+            <h2>São Paulo</h2>
+            <h4>Rua Eustaquio de Souza, 582</h4>
+            <h4>Sapopemba do Sul do Norte</h4>
+            <h4>Tel: (11) 3333-4444</h4>
+        </div>
+    </section>
+    <?php
+    include('rodape.html');
+    ?>
+</body>
+
+</html>
