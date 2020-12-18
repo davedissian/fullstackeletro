8d8cab976a6005defc617cda619d8f616dfd5e55
Diego Avedissian <61209163+davedissian@users.noreply.github.com>
2020-12-18 15:27:12 -0300

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "192.168.1.106";
 $username = "diego";
-$password = "#Recoleta$123a";
+$password = "#Recode$123a";
 $database = "fseletro";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
